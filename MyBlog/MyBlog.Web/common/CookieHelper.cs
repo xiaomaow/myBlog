7331247b07ae5322b0226be05f0af329fbb70cdd
xiaomaow <xiaomaow@163.com>
2018-03-23 10:44:36 +0800

    添加cookieHelper

---
+++ MyBlog/MyBlog.Web/common/CookieHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyBlog.Web.common
+{
+    public class CookieHelper
+    {
+        public static void SetCookie(string key, string value, DateTime? expires = null, string domain = "")
+        {
+            if (null == key) throw new ArgumentNullException("cookie的键不能为空");
+            HttpCookie cookie = new HttpCookie(key, value);
+            cookie.Domain = domain;
+            if (expires != null)
+            {
+                cookie.Expires = Convert.ToDateTime(expires);
+            }
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+    }
+}
