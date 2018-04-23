1b86d0584e5308332e9bbf12d4bc73ab9a2f1432
xiaomaow <xiaomaow@163.com>
2018-04-23 18:55:42 +0800

    首页和文章详细页

--- MyBlog/MyBlog.Web/Controllers/HomeController.cs
+++ MyBlog/MyBlog.Web/Controllers/HomeController.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyBlog.Web.Models;
+using MyBlog.Web.Service;
 
 namespace MyBlog.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        HomeService _service = new HomeService();
+        public ActionResult Index(int typeid=0,int page=0)
         {
-            return View();
+            PageInfo _info = new PageInfo();
+            _info.page = page;
+            _info.page_size = 20;
+            List<artice> _artice_list = _service.GetArticeList(_info, typeid);
+            ViewBag.ArticeList = _artice_list;
+            List<artice_type> _artice_type_list = _service.GetArticeTypeList();
+            ViewBag.ArticeTypeLit = _artice_type_list;
+            return View(ViewBag);
         }
 
-        public ActionResult About()
+        /// <summary>
+        /// 文章详细页面
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult artice(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            artice _artice = _service.GetArtice(id);
+            ViewBag.Item = _artice;
+            List<artice_type> _artice_type_list = _service.GetArticeTypeList();
+            ViewBag.ArticeTypeLit = _artice_type_list;
+            return View(ViewBag);
         }
     }
 }
---
+++ MyBlog/MyBlog.Web/Service/HomeService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyBlog.Web.Models;
+using MyBlog.Web.Service;
+
+namespace MyBlog.Web.Service
+{
+    public class HomeService
+    {
+        public MyBlogContext _context = new MyBlogContext();
+
+        public List<artice> GetArticeList(PageInfo _info, int typeid = 0)
+        {
+            var query = this._context.artice.OrderByDescending(a => a.create_time).ToList();
+            if (typeid > 0)
+            {
+                query = query.Where(a => a.type_id == typeid).ToList();
+            }
+            _info.count = query.Count();
+            query = query.Skip(_info.page_size * _info.page).Take(_info.page_size).ToList();
+            foreach (artice _artice in query)
+            {
+                _artice.type = GetArticeType(_artice.type_id);
+            }
+            return query;
+        }
+
+        public artice_type GetArticeType(int id)
+        {
+            return this._context.artice_type.Where(a => a.id == id).FirstOrDefault();
+        }
+
+        public List<artice_type> GetArticeTypeList()
+        {
+            var query = this._context.artice_type.OrderByDescending(a => a.id).ToList();
+            return query;
+        }
+
+        public artice GetArtice(int id)
+        {
+            var query = this._context.artice.Where(a => a.id == id).FirstOrDefault();
+            if (query != null)
+            {
+                query.type = GetArticeType(query.type_id);
+            }
+            return query;
+        }
+    }
+}
